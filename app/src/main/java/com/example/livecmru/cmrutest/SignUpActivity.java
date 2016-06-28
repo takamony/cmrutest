dddb0f42c234d10d55d5d0d16bf27510adfaa9e5
Jassada <ctak.jas@gmail.com>
2016-06-28 09:44:47 +0700

    alert show

--- app/src/main/java/com/example/livecmru/cmrutest/SignUpActivity.java
+++ app/src/main/java/com/example/livecmru/cmrutest/SignUpActivity.java
@@ -2,6 +2,7 @@ package com.example.livecmru.cmrutest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
@@ -12,7 +13,7 @@ public class SignUpActivity extends AppCompatActivity {
     private EditText nameEditText, passwordEditText, userEditText;
     private RadioGroup radioGroup;
     private RadioButton avata0RadioButton, avata1RadioButton, avata2RadioButton, avata3RadioButton, avata4RadioButton;
-
+    private String namestring, userstring, passwordstring;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,5 +33,15 @@ public class SignUpActivity extends AppCompatActivity {
 
     } //Main method
 
+    public void clickSignUpSign(View view) {
+        namestring = nameEditText.getText().toString().trim();
+        userstring = userEditText.getText().toString().trim();
+        passwordstring = passwordEditText.getText().toString().trim();
+        //Check Space
+        if (namestring.equals("") || userstring.equals("") || passwordstring.equals("")) {
 
+            MyAlert myAlert = new MyAlert();
+            myAlert.myDialog(this, "มีช่องว่าง","กรอกให้ครบ");
+        }
+        }
 } //Main Class
