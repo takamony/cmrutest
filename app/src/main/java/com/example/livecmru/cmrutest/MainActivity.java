dd6bd2f4d85902c6e507b43f8502268007a9e51f
Jassada <ctak.jas@gmail.com>
2016-06-28 16:01:50 +0700

    SignIn

--- app/src/main/java/com/example/livecmru/cmrutest/MainActivity.java
+++ app/src/main/java/com/example/livecmru/cmrutest/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.livecmru.cmrutest;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
     private static final String urlLogo = "http://swiftcodingthai.com/cmru/cmru_logo.png";
     private ImageView imageView;
     private EditText userEditText, passwordEditText;
+    private String userString, passwordString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,10 +24,34 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //
-        ImageView = (ImageView) findViewById(R.id.imageView6);
+        imageView = (ImageView) findViewById(R.id.imageView6);
         userEditText = (EditText) findViewById(R.id.editText4);
         passwordEditText = (EditText) findViewById(R.id.editText5);
+
+        //Load Logo
+        Picasso.with(this).load(urlLogo).resize(150,180).into(imageView);
     } //Main Method
+
+    //create innet class
+    private class SynUser extends AsyncTask<Void, Void, String> {
+
+
+        @Override
+        protected String doInBackground(Void... voids) {
+            return null;
+        }
+    }//SynUser Class
+
+    public void clickSignIn(View view) {
+
+        userString = userEditText.getText().toString().trim();
+        passwordString = passwordEditText.getText().toString().trim();
+
+        if (userString.equals("") || passwordString.equals("")) {
+            MyAlert myAlert = new MyAlert();
+            myAlert.myDialog(this, "No No","no no");
+        }
+    }
 
     public void clickSignUpMain(View view){
         startActivity(new Intent(MainActivity.this,SignUpActivity.class));
